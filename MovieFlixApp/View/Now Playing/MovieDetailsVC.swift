ae93abcd3f46b4ea0ac7b38f1b8bd60e53507ac8
sandesh271991 <sardar.sandesh@gmail.com>
2020-05-16 19:03:31 +0530

    Movie details and UI improvment

--- MovieFlixApp/View/Now Playing/MovieDetailsVC.swift
+++ MovieFlixApp/View/Now Playing/MovieDetailsVC.swift
@@ -11,10 +11,18 @@ import UIKit
 class MovieDetailsVC: UIViewController {
 
     var movieDetails: Result?
+    
+    @IBOutlet weak var imgMovieBG: UIImageView!
+    
+    @IBOutlet weak var lblMovieOverview: UILabel!
+    @IBOutlet weak var lblMovieTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(movieDetails?.title)
+        imgMovieBG.loadImageUsingCache(withUrl: "https://image.tmdb.org/t/p/w342/\(movieDetails?.posterPath ?? "")")
+        lblMovieTitle.text = movieDetails?.title
+        lblMovieOverview.text = movieDetails?.overview
+        print("https://image.tmdb.org/t/p/w342/\(movieDetails?.posterPath ?? "")")
         // Do any additional setup after loading the view.
     }
 
